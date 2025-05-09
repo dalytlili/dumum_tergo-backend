564498a54b481c3c4f6601901f51ead98546c1da
dalytlili <123658659+dalytlili@users.noreply.github.com>
2025-05-09 15:29:42 +0100

    Update multer-config.js

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,35 +1,23 @@
-import multer, { diskStorage } from "multer";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+import cloudinary from './cloudinaryConfig.js';
 
-// MIME types for supported image formats
-const MIME_TYPES = {
-    "image/jpg": "jpg",
-    "image/jpeg": "jpg",
-    "image/png": "png",
-};
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'camping', // Optionnel
+    allowed_formats: ['jpg', 'jpeg', 'png'], // Formats autorisés
+    transformation: [
+        { width: 800, crop: 'limit', quality: 'auto' }, // Compression automatique
+        { fetch_format: 'auto' }
+      ],    public_id: (req, file) => {
+      return `img-${Date.now()}`; // Nom unique
+    },
+  },
+});
 
-// Exporting a middleware function that configures multer for handling file uploads
-export default multer({
-    // Configuring multer with disk storage
-    storage: diskStorage({
-        // Setting the destination for uploaded files
-        destination: (req, file, callback) => {
-            // Getting the current directory using dirname and fileURLToPath
-            const __dirname = dirname(fileURLToPath(import.meta.url));
-
-            // Callback to specify the destination path
-            callback(null, join(__dirname, "../public/images"));
-        },
-        // Setting the filename for uploaded files
-        filename: (req, file, callback) => {
-            // Getting the file extension based on MIME type
-            const extension = MIME_TYPES[file.mimetype];
-
-            // Callback to specify the filename with a timestamp and extension
-            callback(null, Date.now() + "." + extension);
-        },
-    }),
-    // Setting size limits for uploaded files (512 KB in this case)
-    limits: 512 * 1024,
-}).single("image");
+export default multer({ 
+  storage: storage,
+  limits: {     fileSize: 5 * 1024 * 1024 // Augmenté à 5MB
+  } // 512 KB
+}).single('image');
